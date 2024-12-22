8e9b9d73f050a9ff82c3528c10c364b91f2e455e
NunziaLampitelli <nunzia.lampitelli@outlook.com>
2024-12-22 16:17:42 +0100

    tried fixing video homepage

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ window.addEventListener("scroll", function() {
       button.classList.remove("show"); 
   }
 });
+
+
+window.addEventListener('load', function() {
+  // Rimuove la classe vp-center dal video se presente
+  const vpCenterElement = document.querySelector('.vp-center');
+  if (vpCenterElement) {
+      vpCenterElement.classList.remove('vp-center');
+  }
+
+  // Forza la larghezza e l'altezza dell'iframe
+  const videoIframe = document.querySelector('iframe');
+  if (videoIframe) {
+      videoIframe.style.width = '50vw';
+      videoIframe.style.height = '50vh';
+  }
+});
